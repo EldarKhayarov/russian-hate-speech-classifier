190df1e5aa61152fa85b1f939fc35901897ea012
Eldar Khaiarov <eldar@niuitmo.ru>
2023-06-07 07:45:56 +0300

    refactored data preprocessing in API

--- api_app/services.py
+++ api_app/services.py
@@ -1,6 +1,26 @@
 import re
 
-from src.dataset_preprocessing import normalize_phrase_fab
+import nltk
+
+nltk.download("stopwords")
+
+from nltk.corpus import stopwords
+from pymorphy2 import MorphAnalyzer
+
+
+stopwords_list = stopwords.words("russian")
+morph_analyzer = MorphAnalyzer()
+
+
+def normalize_phrase(phrase: str) -> str:
+    phrase = " ".join(
+        [
+            morph_analyzer.normal_forms(word)[0]
+            for word in phrase.split()
+            if phrase not in stopwords_list
+        ]
+    )
+    return phrase
 
 
 def preprocess_phrase(phrase: str) -> str:
@@ -8,7 +28,7 @@ def preprocess_phrase(phrase: str) -> str:
     phrase = re.sub(r"([^ а-яё])|(\d)", " ", phrase)
     phrase = re.sub(r"\s{2,}", " ", phrase)
     phrase = re.sub(r"(^ )|( $)", "", phrase)
-    phrase = normalize_phrase_fab()(phrase)
+    phrase = normalize_phrase(phrase)
     return phrase
 
 
